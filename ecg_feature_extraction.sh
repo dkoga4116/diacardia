41a3f620f96779c47344acf4064bb3d655e67563
Daisuke <Daisuke@Daisukes-MacBook-Pro-2.local>
2024-12-18 20:45:18 +0900

    updated

--- ecg_feature_extraction.sh
+++ ecg_feature_extraction.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 # The following variables must be set before running the script
-input_directory_path='' # Path to the directory containing the ECG data files
-output_directory_path='' # Path to the directory where the extracted features will be saved
-voltage_unit= # The voltage unit of the ECG data in microvolts
-sampling_frequency= # The sampling frequency of the ECG data in Hz
+input_directory_path='./ECG_samples' # Path to the directory containing the ECG data files
+output_directory_path='./ecg_features' # Path to the directory where the extracted features will be saved
+voltage_unit=4.88 # The voltage unit of the ECG data in microvolts (e.g. 4.88 for our original dataset)
+sampling_frequency=500 # The sampling frequency of the ECG data in Hz (e.g. 500 for our original dataset)
 
 # Run the Python script for ECG feature extraction
 python ecg_feature_extraction.py \
